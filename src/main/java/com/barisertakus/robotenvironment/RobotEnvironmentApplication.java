b62d9d79b8800d6a6a78c824ed506a217bfabae9
Barış Ertakuş <bariserta@gmail.com>
2022-04-30 22:05:21 +0300

    Initial commit

---
+++ src/main/java/com/barisertakus/robotenvironment/RobotEnvironmentApplication.java
@@ -0,0 +1,13 @@
+package com.barisertakus.robotenvironment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobotEnvironmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RobotEnvironmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/barisertakus/robotenvironment/RobotEnvironmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barisertakus.robotenvironment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobotEnvironmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
